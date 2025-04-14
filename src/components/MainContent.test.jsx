57cbbe5ecd1be5f787c23f942d27b59705289de6
indrajith1984 <fleetmandevops84@gmail.com>
2025-04-14 15:08:49 +0530

    Added a new comment

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -6,6 +6,7 @@ import MainContent from './MainContent';
 
 // My tests!
 // Add yet another comment
+// Added an important comment
 
 describe('MainContent', () => {
   it('should render a button', () => {
